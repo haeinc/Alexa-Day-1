d80f9d5bf918d12ffd1025e8c3a77117dd4e50bd
haeinc <62230101+haeinc@users.noreply.github.com>
2022-07-13 09:11:33 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Hello World")
+print("My name is Hannah")
+print("Hello " + "World")
+print("Hello" + " " + "World")
+
+myName = "Hannah" # String
+myBirthMonth = 2 # Integer
+myBirthdate = "12" # String
+pi = 3.14 # Float
+isHungry = False # Boolean
+isFull = True
+
+num = 21 # integer
+num2 = str(num) # string
+
+num3 = "455" # String
+num4 = int(num3) # Integer
+
